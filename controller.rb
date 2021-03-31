2048bcbedfc8379cd498e9f59ef8be759feeff7d
Begana Choi <begana001.jpg@gmail.com>
2021-03-31 19:41:23 +0200

    feat(controller): define skeleton CRUD methods

--- controller.rb
+++ controller.rb
@@ -1,8 +1,22 @@
-require_relative 'cookbook'
+require_relative 'recipe'
+require_relative 'view'
 
 class Controller
   def initialize(cookbook)
     @cookbook = cookbook
     @view = View.new
+  end
+
+  def list
+    @cookbook.all
+  end
+
+  def create
+    recipe = Recipe.new(name, description)
+    @cookbook.add(recipe)
+  end
+
+  def destroy
+    @cookbook.delete(index)
   end
 end
